b37cb561a1be679e78757a451d03012ef3710e4d
ReinheimerPiano <pianomatheus@gmail.com>
2020-07-31 14:57:46 -0300

    alter

---
+++ fundamentos-react/src/components/basics/Primeiro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <h3>Teste</h3>
+            <p>Seja bem vindo ao meu primeiro componente</p>
+        </div>
+    )
+}
